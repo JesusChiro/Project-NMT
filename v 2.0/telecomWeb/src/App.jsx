eda892e5719a3fec25d042ac6d1dc33512b3734f
JesusChiro <jesus_chiroque@hotmail.com>
2024-01-16 16:52:37 -0500

    adding component <Route/>

--- v 2.0/telecomWeb/src/App.jsx
+++ v 2.0/telecomWeb/src/App.jsx
@@ -1,37 +1,28 @@
-import { AboutUs } from './components/About/About.jsx'
 import { Router } from './Router.jsx'
-import './styles.css'
+import { Route } from './Route.jsx'
 import { Home } from './components/Home/Home.jsx'
+import { AboutUs } from './components/About/About.jsx'
 import { Contact } from './components/Contact/Contact.jsx'
 import Page404 from './components/404.jsx'
 import SearchPage from './components/Search.jsx'
+import './styles.css'
 
 const appRoutes = [
-  {
-    path: '/',
-    Component: Home,
-  },
-  {
-    path: '/about',
-    Component: AboutUs,
-  },
-  {
-    path: '/contact',
-    Component: Contact,
-  },
   {
     path: '/search/:query',
     Component: SearchPage
   }
 ]
 
-
 export function App() {
-
   return (
     <>
       <div className='allWeb'>
-        <Router routes={appRoutes} defaultComponent={Page404} />
+        <Router routes={appRoutes} defaultComponent={Page404}>
+          <Route path='/' Component={Home} />
+          <Route path='/about' Component={AboutUs} />
+          <Route path='/contact' Component={Contact} />
+        </Router >
       </div>
     </>
   )
--- v 2.0/telecomWeb/src/Router.jsx
+++ v 2.0/telecomWeb/src/Router.jsx
@@ -1,8 +1,7 @@
 import { EVENTS } from "./consts"
-import { useState, useEffect } from "react"
+import { useState, useEffect, Children } from "react"
 import { match } from 'path-to-regexp'
-export function Router({ routes = [], defaultComponent: DefaultComponent = () =>
-    <h1>404</h1> }) {
+export function Router({ children, routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
     const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
     useEffect(() => {
@@ -18,9 +17,17 @@ export function Router({ routes = [], defaultComponent: DefaultComponent = () =>
         }
     }, [])
     let routeParams = {}
+    //add routes from children <Route/> components
 
+    const routesFromChildren = Children.map(children, ({ props, type }) => {
+        const { name } = type
 
-    const Page = routes.find(({ path }) => {
+        const isRoute = name === 'Route'
+
+        return isRoute ? props : null
+    })
+    const routesToUse = routes.concat(routesFromChildren)
+    const Page = routesToUse.find(({ path }) => {
         if (path === currentPath) return true
         const matcherUrl = match(path, { decode: decodeURIComponent })
         //hemos usado path-to-regexp
