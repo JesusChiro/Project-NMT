84447c0841ef28ba2816f0ea33ab11e52f2c3efe
JesusChiro <jesus_chiroque@hotmail.com>
2024-01-18 13:49:35 -0500

    web service finish

--- v 2.0/telecomWeb/src/components/Services/Services.jsx
+++ v 2.0/telecomWeb/src/components/Services/Services.jsx
@@ -4,38 +4,68 @@ import './Services.css'
 
 const SERVICES = [
     {
-        img: 'https://mecanetperu.com/blog/wp-content/uploads/2020/03/beneficios-de-instalacion-de-cableado-estructurado.jpg',
-        title: 'Web Design',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat, quasi, quidem, quibusdam, quod, quaerat, quasi.'
+        img: 'https://i.postimg.cc/7YdpQSXQ/telecity-tenue.jpg',
+        title: 'Ingeniería de Data-Center',
+        text: `Esta Solución que NMT implementa para las instalaciones de Data Center se inició aprovechando las innovaciones 
+        tecnológicas y el gran crecimiento de la industria. Impartiendo una filosofía de diseño flexible, mixto y modular, 
+        la solución se utiliza para construir Data Center con costos efectivos de los clientes, cubriendo la consultoría, 
+        planificación, diseño, implementación y la optimización de las instalaciones del DC.`
     },
     {
-        img: 'https://mecanetperu.com/blog/wp-content/uploads/2020/03/beneficios-de-instalacion-de-cableado-estructurado.jpg',
-        title: 'Programming',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat, quasi, quidem, quibusdam, quod, quaerat, quasi.'
+        img: 'https://i.postimg.cc/44c16MnM/12.jpg',
+        title: 'Sistemas de Deteccion de Incendios',
+        text: `Ofrecemos a nuestros clientes un servicio profesional de protección y extinción de incendios adecuado al entorno 
+        que se requiere proteger. La misión que tenemos inicia de una grandísima estrategia plasmada en una ingeniería de Campo,
+        el suministro de equipos y materiales con altos estándares calidad, la implementación del sistema con personal calificado,
+        el gerenciamiento del mismo por profesionales selectos y un permanente servicio post venta que permitirá mantener los sistemas 
+        en buen estado durante el ciclo de vida del equipo.`
     },
     {
-        img: 'https://mecanetperu.com/blog/wp-content/uploads/2020/03/beneficios-de-instalacion-de-cableado-estructurado.jpg',
-        title: 'IT',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat, quasi, quidem, quibusdam, quod, quaerat, quasi.'
+        img: 'https://i.postimg.cc/G2ML5zMs/77675535.jpg',
+        title: 'Sistemas de Control de Energia',
+        text: `Puesta a tierra de los equipos eléctricos Nuestro principal propósito es eliminar los peligros que ocurre cuando hay 
+        malas instalaciones eléctricas en las construcciones de nuestros actuales y futuros clientes. Además, también vendemos 
+        equipo UPS para prevenir los cortes de energía que influyen en los futuros problemas de equipos de un Datacenter. Por ende, 
+        nos preocupamos por el bienestar de los DC de nuestros clientes para que no haya problemas durante su red no tenga ningún 
+        inconveniente si sucede algún desperfecto durante su alimentación eléctrica.`
     },
     {
-        img: 'https://mecanetperu.com/blog/wp-content/uploads/2020/03/beneficios-de-instalacion-de-cableado-estructurado.jpg',
-        title: 'Backend',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat, quasi, quidem, quibusdam, quod, quaerat, quasi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat, quasi, quidem, quibusdam, quod, quaerat, quasi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat, quasi, quidem, quibusdam, quod, quaerat, quasi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat, quasi, quidem, quibusdam, quod, quaerat, quasi.'
+        img: 'https://i.postimg.cc/XJGH7sbd/cctv.jpg',
+        title: 'Sistema CCTV',
+        text: `Brindamos el servicio de venta e instalacion de equipos de Computo para su empresa o negocio con su respectivo soporte 24/7.`
     },
     {
-        img: 'https://mecanetperu.com/blog/wp-content/uploads/2020/03/beneficios-de-instalacion-de-cableado-estructurado.jpg',
-        title: 'Programming',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quaerat, quasi, quidem, quibusdam, quod, quaerat, quasi.'
+        img: 'https://i.postimg.cc/PfLJTY8H/best-home-office-set-up-for-remote-workers-1648516920.jpg',
+        title: 'Equipos de Computo',
+        text: `Brindamos el servicio de venta e instalacion de equipos de Computo para su empresa o negocio con su respectivo soporte 24/7.`
+    },
+    {
+        img: 'https://i.postimg.cc/VvzKBmtB/networkelements3.jpg',
+        title: 'Network Devices',
+        text: `Ofrecemos al cliente una gama selecta de equipos de redes de acuerdo a las necesidades de su empresa con equipo ya sea
+        para sistemas wired o wireless.`
+    },
+    {
+        img: 'https://i.postimg.cc/R0XhVTRS/proyecto-ingenieria.jpg',
+        title: 'Ingeniería de Proyectos',
+        text: `Brindamos el diseño e implementacion de Centros de Datos para el manejo completo de su red y monitoreo de servicios de servidores.`
+    },
+    {
+        img: 'https://i.postimg.cc/1t0bGW86/crear-pagina-web-gratis-img-header.jpg',
+        title: 'Creación de Sistemas Web',
+        text: 'Realizamos el servicio de paginas y sistemas web de acuerdo a la necesidad del cliente.'
+    },
+    {
+        img: 'https://i.postimg.cc/SxkRcKDt/Ejemplo1.jpg',
+        title: 'Instalación de Digital Signage',
+        text: 'Ofrecemos la instalacion de pantallas para cartelería Digital, videowall, salas de videoconferencia.'
     }
 ]
 export function Services() {
-
     return (
         <>
             <Navbarr />
             <div className='Card__container'>
-                {/* <Container fluid="md" style={{ background: 'white' }}> */}-
                 {
                     SERVICES.map(({ img, title, text }) => (
                         <>
@@ -50,7 +80,6 @@ export function Services() {
                         </>
                     ))
                 }
-                {/* </Container> */}
             </div >
             <Footer />
         </>
