80109ca26aad8f4be887de89c044f634bc7e6e56
JesusChiro <jesus_chiroque@hotmail.com>
2024-02-01 18:54:19 -0500

    move all constanst to const.js

--- v 2.0/telecomWeb/src/components/Services/Services.jsx
+++ v 2.0/telecomWeb/src/components/Services/Services.jsx
@@ -1,66 +1,7 @@
 import { Footer } from '../Footer/Footer.jsx'
 import { Navbarr } from '../Navbar/Navbar.jsx';
+import { SERVICES } from '../../utils/consts.js';
 import './Services.css'
-
-const SERVICES = [
-    {
-        img: 'https://i.postimg.cc/7YdpQSXQ/telecity-tenue.jpg',
-        title: 'Ingeniería de Data-Center',
-        text: `Esta Solución que NMT implementa para las instalaciones de Data Center se inició aprovechando las innovaciones 
-        tecnológicas y el gran crecimiento de la industria. Impartiendo una filosofía de diseño flexible, mixto y modular, 
-        la solución se utiliza para construir Data Center con costos efectivos de los clientes, cubriendo la consultoría, 
-        planificación, diseño, implementación y la optimización de las instalaciones del DC.`
-    },
-    {
-        img: 'https://i.postimg.cc/44c16MnM/12.jpg',
-        title: 'Sistemas de Deteccion de Incendios',
-        text: `Ofrecemos a nuestros clientes un servicio profesional de protección y extinción de incendios adecuado al entorno 
-        que se requiere proteger. La misión que tenemos inicia de una grandísima estrategia plasmada en una ingeniería de Campo,
-        el suministro de equipos y materiales con altos estándares calidad, la implementación del sistema con personal calificado,
-        el gerenciamiento del mismo por profesionales selectos y un permanente servicio post venta que permitirá mantener los sistemas 
-        en buen estado durante el ciclo de vida del equipo.`
-    },
-    {
-        img: 'https://i.postimg.cc/G2ML5zMs/77675535.jpg',
-        title: 'Sistemas de Control de Energia',
-        text: `Puesta a tierra de los equipos eléctricos Nuestro principal propósito es eliminar los peligros que ocurre cuando hay 
-        malas instalaciones eléctricas en las construcciones de nuestros actuales y futuros clientes. Además, también vendemos 
-        equipo UPS para prevenir los cortes de energía que influyen en los futuros problemas de equipos de un Datacenter. Por ende, 
-        nos preocupamos por el bienestar de los DC de nuestros clientes para que no haya problemas durante su red no tenga ningún 
-        inconveniente si sucede algún desperfecto durante su alimentación eléctrica.`
-    },
-    {
-        img: 'https://i.postimg.cc/XJGH7sbd/cctv.jpg',
-        title: 'Sistema CCTV',
-        text: `Brindamos el servicio de venta e instalacion de equipos de Computo para su empresa o negocio con su respectivo soporte 24/7.`
-    },
-    {
-        img: 'https://i.postimg.cc/PfLJTY8H/best-home-office-set-up-for-remote-workers-1648516920.jpg',
-        title: 'Equipos de Computo',
-        text: `Brindamos el servicio de venta e instalacion de equipos de Computo para su empresa o negocio con su respectivo soporte 24/7.`
-    },
-    {
-        img: 'https://i.postimg.cc/VvzKBmtB/networkelements3.jpg',
-        title: 'Network Devices',
-        text: `Ofrecemos al cliente una gama selecta de equipos de redes de acuerdo a las necesidades de su empresa con equipo ya sea
-        para sistemas wired o wireless.`
-    },
-    {
-        img: 'https://i.postimg.cc/R0XhVTRS/proyecto-ingenieria.jpg',
-        title: 'Ingeniería de Proyectos',
-        text: `Brindamos el diseño e implementacion de Centros de Datos para el manejo completo de su red y monitoreo de servicios de servidores.`
-    },
-    {
-        img: 'https://i.postimg.cc/1t0bGW86/crear-pagina-web-gratis-img-header.jpg',
-        title: 'Creación de Sistemas Web',
-        text: 'Realizamos el servicio de paginas y sistemas web de acuerdo a la necesidad del cliente.'
-    },
-    {
-        img: 'https://i.postimg.cc/SxkRcKDt/Ejemplo1.jpg',
-        title: 'Instalación de Digital Signage',
-        text: 'Ofrecemos la instalacion de pantallas para cartelería Digital, videowall, salas de videoconferencia.'
-    }
-]
 export function Services() {
     return (
         <>
--- v 2.0/telecomWeb/src/utils/consts.js
+++ v 2.0/telecomWeb/src/utils/consts.js
@@ -12,3 +12,181 @@ export const NMT_INFO = {
     DATE_CREATED: 2013,
 
 }
+export const SERVICES = [
+    {
+        img: 'https://i.postimg.cc/7YdpQSXQ/telecity-tenue.jpg',
+        title: 'Ingeniería de Data-Center',
+        text: `Esta Solución que NMT implementa para las instalaciones de Data Center se inició aprovechando las innovaciones 
+        tecnológicas y el gran crecimiento de la industria. Impartiendo una filosofía de diseño flexible, mixto y modular, 
+        la solución se utiliza para construir Data Center con costos efectivos de los clientes, cubriendo la consultoría, 
+        planificación, diseño, implementación y la optimización de las instalaciones del DC.`
+    },
+    {
+        img: 'https://i.postimg.cc/44c16MnM/12.jpg',
+        title: 'Sistemas de Deteccion de Incendios',
+        text: `Ofrecemos a nuestros clientes un servicio profesional de protección y extinción de incendios adecuado al entorno 
+        que se requiere proteger. La misión que tenemos inicia de una grandísima estrategia plasmada en una ingeniería de Campo,
+        el suministro de equipos y materiales con altos estándares calidad, la implementación del sistema con personal calificado,
+        el gerenciamiento del mismo por profesionales selectos y un permanente servicio post venta que permitirá mantener los sistemas 
+        en buen estado durante el ciclo de vida del equipo.`
+    },
+    {
+        img: 'https://i.postimg.cc/G2ML5zMs/77675535.jpg',
+        title: 'Sistemas de Control de Energia',
+        text: `Puesta a tierra de los equipos eléctricos Nuestro principal propósito es eliminar los peligros que ocurre cuando hay 
+        malas instalaciones eléctricas en las construcciones de nuestros actuales y futuros clientes. Además, también vendemos 
+        equipo UPS para prevenir los cortes de energía que influyen en los futuros problemas de equipos de un Datacenter. Por ende, 
+        nos preocupamos por el bienestar de los DC de nuestros clientes para que no haya problemas durante su red no tenga ningún 
+        inconveniente si sucede algún desperfecto durante su alimentación eléctrica.`
+    },
+    {
+        img: 'https://i.postimg.cc/XJGH7sbd/cctv.jpg',
+        title: 'Sistema CCTV',
+        text: `Brindamos el servicio de venta e instalacion de equipos de Computo para su empresa o negocio con su respectivo soporte 24/7.`
+    },
+    {
+        img: 'https://i.postimg.cc/PfLJTY8H/best-home-office-set-up-for-remote-workers-1648516920.jpg',
+        title: 'Equipos de Computo',
+        text: `Brindamos el servicio de venta e instalacion de equipos de Computo para su empresa o negocio con su respectivo soporte 24/7.`
+    },
+    {
+        img: 'https://i.postimg.cc/VvzKBmtB/networkelements3.jpg',
+        title: 'Network Devices',
+        text: `Ofrecemos al cliente una gama selecta de equipos de redes de acuerdo a las necesidades de su empresa con equipo ya sea
+        para sistemas wired o wireless.`
+    },
+    {
+        img: 'https://i.postimg.cc/R0XhVTRS/proyecto-ingenieria.jpg',
+        title: 'Ingeniería de Proyectos',
+        text: `Brindamos el diseño e implementacion de Centros de Datos para el manejo completo de su red y monitoreo de servicios de servidores.`
+    },
+    {
+        img: 'https://i.postimg.cc/1t0bGW86/crear-pagina-web-gratis-img-header.jpg',
+        title: 'Creación de Sistemas Web',
+        text: 'Realizamos el servicio de paginas y sistemas web de acuerdo a la necesidad del cliente.'
+    },
+    {
+        img: 'https://i.postimg.cc/SxkRcKDt/Ejemplo1.jpg',
+        title: 'Instalación de Digital Signage',
+        text: 'Ofrecemos la instalacion de pantallas para cartelería Digital, videowall, salas de videoconferencia.'
+    }
+]
+
+export const ABOUTUS = [
+    {
+        Img: 'https://i.postimg.cc/cLr49TJn/aboutus.png',
+        Alt: 'NMT nosotros',
+        Title: 'Quienes Somos',
+        Description: `En nuestro negocio nos hemos enfocado en Atender, Aconsejar, Manifestar, Conducir a nuestros clientes para crear sólidas
+        y estrechas relaciones con ellos y ser parte de su negocio. Nuestra oferta se encuentra respaldada por aliados tecnológicos - 
+        reconocidos a nivel global - y por nuestra cartera de servicios certificados y garantizados, en manos de un recurso humano totalmente capacitado.`
+    },
+    {
+        Img: 'https://i.postimg.cc/jqpXd3ps/ourmission.png',
+        Alt: 'Misión de la empresa NM TELECOM',
+        Title: 'Nuestra Misión',
+        Description: `Satisfacer las necesidades de nuestros clientes a través de servicios de calidad, brindando soluciones efectivas en áreas de Redes de Telecomunicaciones, 
+        Fibra óptica, Centrales Telefónicas, Sistemas Eléctricos, Sistemas de puesta a tierra y otras actividades afines.`
+    },
+    {
+        Img: 'https://i.postimg.cc/jdyhWQTy/ourvision.png',
+        Title: 'Nuestra Visión',
+        Alt: 'Visión de la empresa NM TELECOM',
+        Description: `Ser la primera opcion de nuestros clientes en servicios en Redes de Telecomunicaciones, Fibra óptica, Centrales Telefónicas, Sistemas Eléctricos, Sistemas de puesta
+        a tierra y otras actividades afines. Líder en el medio, reconocida por su calidad de servicio, atención  personalizada y oportuna.`
+    }
+]
+
+export const CUSTOMERS = [
+    {
+        Id: '1',
+        Img: './img/index/customers/csj.jpg',
+        Alt: 'Cliente de NM TELECOM - Poder Judicial Lima Norte '
+    },
+    {
+        Id: '2',
+        Img: './img/index/customers/incor.png',
+        Alt: 'Cliente de NM TELECOM - Instituto Nacional Cardiológico'
+    },
+    {
+        Id: '3',
+        Img: './img/index/customers/weir.png',
+        Alt: 'Cliente de NM TELECOM - Weir Mineral Perú - VULCO Perú'
+    }, {
+        Id: '4',
+        Img: './img/index/customers/essalud.png',
+        Alt: 'Cliente de NM TELECOM - Seguro Social de Salud'
+    },
+    {
+        Id: '5',
+        Img: './img/index/customers/unp.png',
+        Alt: 'Cliente de NM TELECOM - Poder Judicial Lima Norte '
+    },
+    {
+        Id: '6',
+        Img: './img/index/customers/csj.jpg',
+        Alt: 'Cliente de NM TELECOM - Poder Judicial Lima Norte '
+    },
+    {
+        Id: '7',
+        Img: './img/index/customers/incor.png',
+        Alt: 'Cliente de NM TELECOM - Instituto Nacional Cardiológico'
+    },
+    {
+        Id: '8',
+        Img: './img/index/customers/weir.png',
+        Alt: 'Cliente de NM TELECOM - Weir Mineral Perú - VULCO Perú'
+    }, {
+        Id: '9',
+        Img: './img/index/customers/essalud.png',
+        Alt: 'Cliente de NM TELECOM - Seguro Social de Salud'
+    },
+    {
+        Id: '10',
+        Img: './img/index/customers/unp.png',
+        Alt: 'Cliente de NM TELECOM - Poder Judicial Lima Norte '
+    },
+]
+
+export const PROVIDERS = [
+    {
+        Id: '1',
+        Img: './img/index/proveedores/intcomex.png',
+        Alt: 'Proveedor de NM TELECOM - INTCOMEX '
+    },
+    {
+        Id: '2',
+        Img: './img/index/proveedores/sego.png',
+        Alt: 'Proveedor de NM TELECOM - SEGO '
+    },
+    {
+        Id: '3',
+        Img: './img/index/proveedores/anixter.svg',
+        Alt: 'Proveedor de NM TELECOM - ANIXTER '
+    },
+    {
+        Id: '4',
+        Img: './img/index/proveedores/perfection.svg',
+        Alt: 'Proveedor de NM TELECOM - Perefection '
+    },
+    {
+        Id: '5',
+        Img: './img/index/proveedores/intcomex.png',
+        Alt: 'Proveedor de NM TELECOM - INTCOMEX '
+    },
+    {
+        Id: '6',
+        Img: './img/index/proveedores/sego.png',
+        Alt: 'Proveedor de NM TELECOM - SEGO '
+    },
+    {
+        Id: '7',
+        Img: './img/index/proveedores/anixter.svg',
+        Alt: 'Proveedor de NM TELECOM - ANIXTER '
+    },
+    {
+        Id: '8',
+        Img: './img/index/proveedores/perfection.svg',
+        Alt: 'Proveedor de NM TELECOM - Perefection '
+    },
+]
