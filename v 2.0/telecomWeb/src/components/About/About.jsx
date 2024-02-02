96e88d648b86d249c50640d6b424f4226b443ea4
JesusChiro <jesus_chiroque@hotmail.com>
2024-02-02 14:24:31 -0500

    About us page finished

--- v 2.0/telecomWeb/src/components/About/About.jsx
+++ v 2.0/telecomWeb/src/components/About/About.jsx
@@ -6,32 +6,39 @@ export function AboutUs() {
     return (
         <>
             <Navbarr />
-            <div className="about-us-container">
-                <h1 className="title">Sobre Nosotros</h1>
-                {/* <div className="about-us-img">
-                    <img src="./img/aboutus.jpg" />
-                </div> */}
-                <div className="about-us-text">
-                    <p>
-                        {NMT_INFO.NAME} es un proveedor líder de soluciones de redes innovadoras.
-                        Desde nuestra creación en {NMT_INFO.DATE_CREATED}, hemos estado a la
-                        vanguardia en el diseño, implementación y gestión de infraestructuras de
-                        red seguras y sólidas. Nuestro equipo de profesionales de redes certificados
-                        se dedica a ayudar a las empresas a optimizar sus entornos de TI,
-                        garantizando una conectividad perfecta y una mayor productividad.Cada miembro
-                        de nuestro equipo juega un papel vital en la entrega de los resultados
-                        excepcionales que nuestros clientes esperan de nosotros.
-                        Nos especializamos en una amplia gama de servicios, que incluyen diseño de
-                        redes, seguridad, redes en la nube y más. Nuestra misión es empoderar a
-                        las empresas brindándoles soluciones de red confiables, escalables y seguras
-                        adaptadas a sus necesidades específicas.
-                        Estamos orgullosos de lo que hemos logrado hasta ahora, pero estamos aún más 
-                        emocionados por el futuro. Continuaremos innovando y mejorando en todo lo 
+            <div className="aboutus-container">
+                <h1 className="aboutus-title">Sobre Nosotros</h1>
+                <div className="aboutus-img">
+                    <img src="./img/index/aboutus/image.png" />
+                </div>
+                <div className="aboutus-text">
+                    <h2 className="aboutus-subtitle">¿Quienes somos?</h2>
+                    <p className="aboutus-p">
+                        {NMT_INFO.NAME} es un proveedor líder de soluciones tecnlógicas de redes
+                        innovadoras. Desde nuestra creación en {NMT_INFO.DATE_CREATED}, hemos
+                        estado a la vanguardia en el diseño, implementación y gestión de
+                        infraestructuras de red seguras y sólidas.<br /> Nuestro equipo de
+                        profesionales de redes certificados se dedica a ayudar a las empresas a
+                        optimizar sus entornos de TI, garantizando una conectividad perfecta y
+                        una mayor productividad.Cada miembro de nuestro equipo juega un papel
+                        vital en la entrega de los resultados excepcionales que nuestros clientes
+                        esperan de nosotros. Nos especializamos en una amplia gama de servicios,
+                        que incluyen diseño de redes, seguridad, redes en la nube y más.
+                    </p>
+                    <h2 className="aboutus-subtitle">¿Cuál es nuestra visión?</h2>
+                    <p className="aboutus-p">
+                        Nuestra misión es empoderar a las empresas brindándoles soluciones de red
+                        confiables, escalables y seguras adaptadas a sus necesidades específicas.
+                        Estamos orgullosos de lo que hemos logrado hasta ahora, pero estamos aún más
+                        emocionados por el futuro.<br /> Continuaremos innovando y mejorando en todo lo
                         que hacemos para cumplir y superar las expectativas de nuestros clientes.
-                        Únase a nosotros mientras continuamos dando forma al futuro de las redes.
-                        Gracias por visitar nuestro sitio web. Si tienes alguna pregunta o te gustaría 
-                        saber más sobre nosotros y lo que podemos hacer por ti, no dudes en ponerte en 
-                        contacto con nosotros.
+                    </p>
+                    <h2 className="aboutus-subtitle">¿Cuál es nuestra misión?</h2>
+                    <p className="aboutus-p">
+                        Ser la primera opcion de nuestros clientes en servicios en Redes de
+                        Telecomunicaciones, Fibra Óptica, Centrales Telefónicas, Sistemas Eléctricos,
+                        Sistemas de puesta a tierra y otras actividades afines. Líder en el medio,
+                        reconocida por su calidad de servicio, atención personalizada y oportuna.
                     </p>
                 </div>
             </div>
