cf07513124a765b3dd6f00ac308f863f93d6d464
JesusChiro <jesus_chiroque@hotmail.com>
2024-02-08 12:12:55 -0500

    changing BrowserRouter by HashRouter

--- v 2.0/telecomWeb/src/main.jsx
+++ v 2.0/telecomWeb/src/main.jsx
@@ -2,11 +2,11 @@ import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
 import { App } from './App.jsx'
 import './styles.css'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 )
